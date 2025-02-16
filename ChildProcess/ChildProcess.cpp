16b9f398e1b1914b946e841a1e34271a38cd68c4
IsPorCHen <fazy2019@gmail.com>
2025-02-16 22:14:38 +0500

    ыфва

--- ChildProcess/ChildProcess.cpp
+++ ChildProcess/ChildProcess.cpp
@@ -1,14 +1,41 @@
 ﻿#include <windows.h>
 #include <iostream>
+#include <cstdlib>
 
-int main() {
-    setlocale(0, "rus");
-    HANDLE hParentProcess = GetCurrentProcess();
+int main(int argc, char* argv[]) {
+    setlocale(LC_ALL, "rus");
+
+    std::cout << "Дочерний процесс запущен!" << std::endl;
+
+    if (argc < 2) {
+        std::cerr << "Ошибка: Не передан дескриптор родительского процесса!" << std::endl;
+        return 1;
+    }
+
+    // получаю дескриптор родительского процесса
+    HANDLE hParentProcess = (HANDLE)_strtoui64(argv[1], NULL, 10);
+
+    if (hParentProcess == NULL || hParentProcess == INVALID_HANDLE_VALUE) {
+        std::cerr << "Ошибка: Неверный дескриптор родительского процесса!" << std::endl;
+        return 1;
+    }
+
     DWORD parentProcessID = GetProcessId(hParentProcess);
+    DWORD childProcessID = GetCurrentProcessId();
 
-    std::cout << "ID дочернего процесса: " << GetCurrentProcessId() << std::endl;
+    std::cout << "ID дочернего процесса: " << childProcessID << std::endl;
     std::cout << "ID родительского процесса: " << parentProcessID << std::endl;
-    std::cout << "Адрес псевдодескриптора родительского процесса: " << &hParentProcess << std::endl;
+
+    Sleep(2000);
+
+    if (TerminateProcess(hParentProcess, 0)) {
+        std::cout << "Родительский процесс завершён!" << std::endl;
+    }
+    else {
+        std::cerr << "Ошибка завершения родительского процесса!" << std::endl;
+    }
+
+    Sleep(5000);
 
     return 0;
 }
--- ParentProcess/ParentProcess.cpp
+++ ParentProcess/ParentProcess.cpp
@@ -1,43 +1,47 @@
 ﻿#include <windows.h>
 #include <iostream>
+#include <sstream>
 
 int main() {
     setlocale(0, "rus");
-    DWORD currentProcessID = GetCurrentProcessId();
 
-    std::cout << "ID родительского процесса: " << currentProcessID << std::endl;
+    DWORD parentProcessID = GetCurrentProcessId();
+    HANDLE hParentProcess = GetCurrentProcess();
+    HANDLE hDupProcess = NULL;
 
-    STARTUPINFO si;
-    PROCESS_INFORMATION pi;
+    std::wcout << L"ID родительского процесса: " << parentProcessID << std::endl;
 
-    ZeroMemory(&si, sizeof(si));
-    ZeroMemory(&pi, sizeof(pi));
-
-    si.cb = sizeof(si);
-
-    HANDLE hProcess = GetCurrentProcess();
-
-    if (!CreateProcess(
-        L"C:\\Users\\ipch\\source\\repos\\ParentProcess\\x64\\Debug\\ChildProcess.exe",
-        NULL,
-        NULL,
-        NULL,
-        TRUE,
-        0,
-        NULL,
-        NULL,
-        &si,
-        &pi
-    )) {
-        std::cerr << "Ошибка при создании дочернего процесса. Код ошибки: " << GetLastError() << std::endl;
+    if (!DuplicateHandle(
+        GetCurrentProcess(), hParentProcess,
+        GetCurrentProcess(), &hDupProcess,
+        PROCESS_TERMINATE, TRUE, DUPLICATE_SAME_ACCESS))
+    {
+        std::wcerr << L"Ошибка дублирования дескриптора. Код ошибки: " << GetLastError() << std::endl;
         return 1;
     }
 
-    std::cout << "Дочерний процесс запущен с ID: " << pi.dwProcessId << std::endl;
-    std::cout << "Данные переданы в дочерний процесс." << std::endl;
+    // создаю командную строку для дочернего процесса
+    std::wstringstream commandLine;
+    commandLine << L"ChildProcess.exe " << (uint64_t)hDupProcess;
+    std::wstring commandStr = commandLine.str();
+
+    STARTUPINFO si = { sizeof(STARTUPINFO) };
+    PROCESS_INFORMATION pi;
+
+    if (!CreateProcessW(
+        NULL,
+        &commandStr[0],
+        NULL, NULL, TRUE, CREATE_NEW_CONSOLE, NULL, NULL, &si, &pi))
+    {
+        std::wcerr << L"Ошибка при создании дочернего процесса. Код ошибки: " << GetLastError() << std::endl;
+        return 1;
+    }
+
+    std::wcout << L"Дочерний процесс запущен с ID: " << pi.dwProcessId << std::endl;
 
     WaitForSingleObject(pi.hProcess, INFINITE);
 
+    CloseHandle(hDupProcess);
     CloseHandle(pi.hProcess);
     CloseHandle(pi.hThread);
 
